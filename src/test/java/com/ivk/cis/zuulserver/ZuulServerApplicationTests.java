5054d163fd7638ee844a63409960f97bc690be7d
Ivan Katjounis <ivk.msg+github@gmail.com>
2020-01-01 18:44:25 +0100

    Premier commit

---
+++ src/test/java/com/ivk/cis/zuulserver/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivk.cis.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
